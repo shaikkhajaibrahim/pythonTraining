9ea8119b5ed790fdbb5540f1cd5e52fc56590948
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-09-02 05:55:28 +0530

    new samples added

---
+++ Batch-1/Practice/Input.py
@@ -0,0 +1,15 @@
+print("Enter any Number")
+x=input()
+print(type(x))
+number=int(x)
+#Writing Code to find even number
+#Modulos Operator %
+# number%2
+print(number%2)
+print(number%2==0)
+if number%2==0 :
+    print("number is even")
+else:
+    print("number is odd")
+	
+
---
+++ Batch-1/Practice/SimpleIntrest.py
@@ -0,0 +1,14 @@
+#p*t*r/100
+print("Enter Principle Amount")
+x=input()
+p=float(x)
+print("Enter Time In Years")
+x=input()
+t=float(x)
+print("Enter Rate of Intrest")
+r=float(input())
+
+#Calculate
+si=p*t*r/100
+
+print(si)
